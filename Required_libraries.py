a4edaa07d5ee040e7d39a8a388a13fe95b3ca5ac
Sheshraj Pawar <143826741+sheshrajPawar@users.noreply.github.com>
2024-09-24 11:07:24 +0530

    Create Required_libraries.py

---
+++ Required_libraries.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from scipy import stats
+import tensorflow as tf
+import seaborn as sns
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+%matplotlib inline 
